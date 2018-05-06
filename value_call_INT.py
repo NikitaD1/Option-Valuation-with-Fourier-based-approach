7fe76b81f0908df26aa8ed2d674e282dfccc0937
Nikita Dutta <nikitadutta2000@gmail.com>
2018-05-07 00:13:08 +0530

    updating notebook

--- value_call_INT.py
+++ value_call_INT.py
@@ -8,4 +8,4 @@ mu = -0.2 # expected jump size
 delta = 0.1 # jump size volatility
 int_value = quad(lambda u: integration_function(u, S0, K, T, r, sigma, lamb, mu, delta), 0, 50, limit=250)[0]
 call_value = S0 - np.exp(-r * T) * math.sqrt(S0 * K) / math.pi * int_value
-rint("The value of call option is %8.3f" %call_value)
+print("The value of call option is %8.3f" %call_value)
